2579210a1875fdbfb5e4781a4940228772eed7d5
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-03-23 12:57:44 +0300

    Update hackpro.py

--- hackpro.py
+++ hackpro.py
@@ -1,13 +1,22 @@
-import os, sys
-if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		if sys.argv[1] == "remove":
-			os.system("rm -f .__apikey__.txt")
-			exit(" [!] Succesfull Deleted")
-		else:
-			print(" [?] Wellcome : ")
-			exit(" [!] Run : python Filepro.py remove")
-	try:
-		__import__("libnewup").__main__James()
-	except Exception as e:
-		exit(str(e))
+			
+	GNd.py
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Prohack import lisensi
+    lisensi()
+elif bit == '32bit':
+    from Prohack import lisensi
+    lisensi()	
+			
+
+	
+
+	
+
