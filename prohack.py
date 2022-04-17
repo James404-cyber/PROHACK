82d7de41c45a0df604ed91fc0b50ecd0acd4ba01
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-04-17 13:11:03 +0300

    Add files via upload

---
+++ prohack.py
@@ -0,0 +1,9 @@
+import platform
+
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("Prohackfile").teaching_fix()
+elif 'aarch' in arc:
+	__import__("Prohackfile64").teaching_fix()
+else:
+	exit(f' Unknow device machine {arc}')
