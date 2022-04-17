a4c221c54a1d3b1af10e7d826e8e779a4aa5936f
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-04-17 13:10:49 +0300

    prohack.py

--- prohack.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Prohack").lisensi()
-elif 'aarch' in arc:
-	__import__("Prohack64").lisensi()
-else:
-	exit(f' Unknow device machine {arc}')
