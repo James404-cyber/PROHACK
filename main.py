723e25b90df85b2878057c57020317971ee88f56
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-11 10:55:19 +0300

    main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Prohack")._site_ope_()
-elif 'aarch' in arc:
-	__import__("Prohack64")._site_ope_()
-else:
-	exit(f' Unknow device machine {arc}')
