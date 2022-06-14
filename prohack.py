c00b28ea0796350b266928ae30220b2390b3f25e
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-14 18:34:03 +0300

    Update prohack.py

--- prohack.py
+++ prohack.py
@@ -12,8 +12,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-        __import__("Prohack")._site_ope_()
+        __import__("Prohack")._site_view_
 elif 'aarch' in arc:
-        __import__("Prohack64")._site_ope_()
+        __import__("Prohack64")._site_view_()
 else:
         exit(f' Unknow device machine {arc}')
