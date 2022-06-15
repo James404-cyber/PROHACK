f8ba39a0371c1479ad136b095e04db41ba7a8e62
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-15 09:44:36 +0300

    Update prohack.py

--- prohack.py
+++ prohack.py
@@ -1,19 +1,19 @@
 import platform
 import os
 os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('mkdir /sdcard/PROHACK-DATA')
-except:pass
-try:os.system('mkdir /sdcard/PROHACK-DATA/OK')
-except:pass
-try:os.system('mkdir /sdcard/PROHACK-DATA/CP')
-except:pass
+#os.system('git pull')
+#try:os.system('mkdir /sdcard/PROHACK-DATA')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/OK')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/CP')
+#except:pass
 try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
         __import__("Prohack")._site_view_
 elif 'aarch' in arc:
-        __import__("Prohack64")._site_view_()
+        __import__("io")._site_view_()
 else:
         exit(f' Unknow device machine {arc}')
