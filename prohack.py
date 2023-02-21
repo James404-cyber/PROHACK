76915d3e0bc48c30e18863530613af6d491919e1
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-02-21 12:21:57 +0300

    Update prohack.py

--- prohack.py
+++ prohack.py
@@ -15,6 +15,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
         __import__("Pre").ninex()
 elif 'aarch' in arc:
-        __import__("Pre").ninex()
+        __import__("Fx").ninex()
 else:
         exit(f' Unknow device machine {arc}')
